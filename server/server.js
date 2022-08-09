5d887314f67ce38b6701cd9e8a513586c85aeb3d
AcePizza <stianchrbakke@gmail.com>
2022-08-09 14:10:52 +0200

    configure server and setup test route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 import express from "express";
+import cors from "cors";
+import router from "./routes/users.js";
+
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+app.use("/users", router);
