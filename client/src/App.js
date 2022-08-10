338e040b9604bf571888e103a71e3c73dec88a82
AcePizza <stianchrbakke@gmail.com>
2022-08-10 10:14:28 +0200

    cleaned and installed MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,4 @@
 import "./App.css";
-import TestMUI from "./TestMUI";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import TestMUICard from "./TestMUICard";
 
 function App() {
   const onClickButtonHandeler = () => {};
