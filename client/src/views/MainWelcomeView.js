244f1c080369ca7a942a9f8e8b3726a53c6a240a
AcePizza <stianchrbakke@gmail.com>
2022-08-12 12:49:53 +0200

    added main welcome screen

---
+++ client/src/views/MainWelcomeView.js
@@ -0,0 +1,13 @@
+import { Container } from "@mui/system";
+import React from "react";
+
+function MainWelcomeView() {
+  return (
+    <Container>
+      <h4>Welcome to the spotGuide</h4>
+      <a href="/home">Click here to go home...</a>
+    </Container>
+  );
+}
+
+export default MainWelcomeView;
