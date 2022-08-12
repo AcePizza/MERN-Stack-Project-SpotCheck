d38067ae68e91f7f5761c9a256c865a46990b0af
AcePizza <stianchrbakke@gmail.com>
2022-08-12 15:28:41 +0200

    added basic react router config

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import MainNavbar from "./navigation/MainNavbar";
 import MainSpotListView from "./views/MainSpotListView";
+import { Route, Routes } from "react-router-dom";
+import MainWelcomeView from "./views/MainWelcomeView";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <MainNavbar />
-      <MainSpotListView />
-    </>
+      <Routes>
+        <Route path="home" element={<MainSpotListView />} />
+        <Route path="/" element={<MainWelcomeView />} />
+      </Routes>
+    </div>
   );
 }
 
