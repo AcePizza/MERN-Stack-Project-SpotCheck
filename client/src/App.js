6ca6d17f590b23513d6a346596fb72865d1a39e8
AcePizza <stianchrbakke@gmail.com>
2022-08-15 10:52:21 +0200

    set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import MainNavbar from "./navigation/MainNavbar";
 import MainSpotListView from "./views/MainSpotListView";
 import { Route, Routes } from "react-router-dom";
 import MainWelcomeView from "./views/MainWelcomeView";
+import LoginView from "./views/LoginView";
+import SignupView from "./views/SignupView";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="home" element={<MainSpotListView />} />
         <Route path="/" element={<MainWelcomeView />} />
+        <Route path="login" element={<LoginView />} />
+        <Route path="signup" element={<SignupView />} />
       </Routes>
     </div>
   );
