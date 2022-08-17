dc6dd33de14cfa75bcfdb5ab5d60cb36e8b45153
AcePizza <stianchrbakke@gmail.com>
2022-08-17 15:26:28 +0200

    added interactive route

--- server/controller/spotController.js
+++ server/controller/spotController.js
@@ -4,7 +4,7 @@ const getAllSpots = async (req, res) => {
   try {
     const allSpots = await SpotModal.find({});
     if (allSpots.length === 0) {
-      res.status(200).json({
+      res.status(201).json({
         msg: "No users registered",
       });
     } else {
@@ -21,4 +21,56 @@ const getAllSpots = async (req, res) => {
   }
 };
 
-export { getAllSpots };
+const getBoroughs = async (req, res) => {
+  const { rating } = req.query;
+
+  // Conditinal that checks is there is a rating in URL and if found uses it in the search
+  if (rating) {
+    try {
+      const requestedBoroughs = await SpotModal.find({
+        location: req.params.boroughsBerlin,
+        rating: { $gte: rating },
+      }).exec();
+
+      if (requestedBoroughs.length === 0) {
+        res.status(201).json({
+          msg: "Nothing found",
+        });
+      } else {
+        res.status(200).json({
+          requestedBoroughs,
+          number: requestedBoroughs.length,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        error: error,
+        msg: "Server Failed",
+      });
+    }
+  } else {
+    try {
+      const requestedBoroughs = await SpotModal.find({
+        location: req.params.boroughsBerlin,
+      }).exec();
+
+      if (requestedBoroughs.length === 0) {
+        res.status(201).json({
+          msg: "Nothing found",
+        });
+      } else {
+        res.status(200).json({
+          requestedBoroughs,
+          number: requestedBoroughs.length,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        error: error,
+        msg: "Server Failed",
+      });
+    }
+  }
+};
+
+export { getAllSpots, getBoroughs };
--- server/routes/spotRoute.js
+++ server/routes/spotRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllSpots } from "../controller/spotController.js";
+import { getAllSpots, getBoroughs } from "../controller/spotController.js";
 
 const spotRouter = express.Router();
 
 spotRouter.get("/all", getAllSpots);
+spotRouter.get("/:boroughsBerlin", getBoroughs);
 
 export default spotRouter;
