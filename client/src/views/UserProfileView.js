d95018cc0f33f316420c5d01e89f9c36dd550e86
AcePizza <stianchrbakke@gmail.com>
2022-08-25 13:03:16 +0200

    created user profile page

--- client/src/views/UserProfileView.js
+++ client/src/views/UserProfileView.js
@@ -1,7 +1,53 @@
 import React from "react";
+import Avatar from "@mui/material/Avatar";
+import TextField from "@mui/material/TextField";
+import { Container } from "@mui/system";
+import { Divider, Grid } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import Button from "@mui/material/Button";
 
 function UserProfileView() {
-  return <div>UserProfileView</div>;
+  return (
+    <Container>
+      <Avatar
+        alt="Remy Sharp"
+        src="/static/images/avatar/1.jpg"
+        sx={{ width: 56, height: 56 }}
+      />
+      <br />
+      <IconButton color="primary" aria-label="upload picture" component="label">
+        <input hidden accept="image/*" type="file" />
+        <PhotoCamera />
+      </IconButton>
+      <Divider />
+      <br />
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <TextField
+            required
+            id="firstname"
+            label="First Name"
+            defaultValue="Hello World"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            required
+            id="lastname"
+            label="Last Name"
+            defaultValue="Hello World"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={6}></Grid>
+        <Grid item xs={6}>
+          <Button variant="contained">Update</Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default UserProfileView;
