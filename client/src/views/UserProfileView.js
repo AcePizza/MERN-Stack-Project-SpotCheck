145938c0369470ed2cc0bf0b1ede1b5337070e93
AcePizza <stianchrbakke@gmail.com>
2022-08-28 17:53:59 +0200

    added code for updating user profile

--- client/src/views/UserProfileView.js
+++ client/src/views/UserProfileView.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import TextField from "@mui/material/TextField";
 import { Container } from "@mui/system";
@@ -6,64 +6,178 @@ import { Divider, Grid } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import Button from "@mui/material/Button";
+import LoadingPleaseWait from "../components/LoadingPleaseWait";
 
 function UserProfileView() {
+  const [profileData, setProfileData] = useState(null);
+  const [fileObject, setFileOject] = useState(null);
+  const [imageURL, setImageURL] = useState(null);
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
+
+  const firstnameChangeHandler = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const lastnameChangeHandler = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const getLoggedInUser = async () => {
+    const getToken = localStorage.getItem("token");
+    const token = `"Bearer ${getToken}"`;
+
+    var myHeaders = new Headers();
+    myHeaders.append(
+      "Authorization",
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MmZlODcxYTA3ZmY4NDE4NGRmZTE3MDIiLCJpYXQiOjE2NjE1MTgxODQsImV4cCI6MTY2MTk1MDE4NH0.cjzuTMn2y-oWSKxu_z1nFcBxpialVD4Uuy7knP6ejnY"
+    );
+
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:5000/users/findoneuser",
+        requestOptions
+      );
+      const results = await response.json();
+      setProfileData(results);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const handleFile = (e) => {
+    setFileOject(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("image", fileObject);
+
+    const imageRequestOptions = {
+      method: "POST",
+      body: formData,
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:5000/users/imageupload",
+        imageRequestOptions
+      );
+      const result = await response.json();
+      setImageURL(result.imageURL);
+      if (imageURL) {
+        try {
+          const myHeaders = new Headers();
+          myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+          const urlencoded = new URLSearchParams();
+          urlencoded.append("firstname", firstName);
+          urlencoded.append("lastname", lastName);
+          urlencoded.append("emailaddress", profileData.emailaddress);
+          urlencoded.append("image", imageURL);
+
+          const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: urlencoded,
+            redirect: "follow",
+          };
+
+          const profileResponse = await fetch(
+            "http://localhost:5000/users/update",
+            requestOptions
+          );
+          const profileResults = await profileResponse.json();
+          console.log(profileResults);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getLoggedInUser();
+  }, []);
+
   return (
     <Container>
-      <Grid container spacing={2}>
-        <Grid item xs={4}></Grid>
-        <Grid item xs={4}>
-          <Avatar
-            alt="Remy Sharp"
-            src="/static/images/avatar/1.jpg"
-            sx={{ width: 56, height: 56 }}
-          />
-        </Grid>
-        <Grid item xs={4}></Grid>
-      </Grid>
+      {!profileData ? (
+        <LoadingPleaseWait />
+      ) : (
+        <>
+          <Grid container spacing={2}>
+            <Grid item xs={4}></Grid>
+            <Grid item xs={4}>
+              <Avatar
+                alt="Remy Sharp"
+                src={profileData.image}
+                sx={{ width: 56, height: 56 }}
+              />
+            </Grid>
+            <Grid item xs={4}></Grid>
+          </Grid>
 
-      <br />
-      <Grid container spacing={2}>
-        <Grid item xs={4}></Grid>
-        <Grid item xs={4}>
-          <IconButton
-            color="primary"
-            aria-label="upload picture"
-            component="label"
-          >
-            <input hidden accept="image/*" type="file" />
-            <PhotoCamera />
-          </IconButton>
-        </Grid>
-        <Grid item xs={4}></Grid>
-      </Grid>
+          <br />
+          <Grid container spacing={2}>
+            <Grid item xs={4}></Grid>
+            <Grid item xs={4}>
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="label"
+                onChange={handleFile}
+              >
+                <input hidden accept="image/*" type="file" />
 
-      <Divider />
-      <br />
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <TextField
-            required
-            id="firstname"
-            label="First Name"
-            defaultValue="Hello World"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            required
-            id="lastname"
-            label="Last Name"
-            defaultValue="Hello World"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={6}></Grid>
-        <Grid item xs={6}>
-          <Button variant="contained">Update</Button>
-        </Grid>
-      </Grid>
+                <PhotoCamera />
+              </IconButton>
+            </Grid>
+            <Grid item xs={4}></Grid>
+          </Grid>
+
+          <Divider />
+          <br />
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                required
+                id="firstname"
+                label="First Name"
+                defaultValue={profileData.firstname}
+                variant="standard"
+                onChange={firstnameChangeHandler}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                required
+                id="lastname"
+                label="Last Name"
+                defaultValue={profileData.lastname}
+                variant="standard"
+                onChange={lastnameChangeHandler}
+              />
+            </Grid>
+            <Grid item xs={6}></Grid>
+            <Grid item xs={6}>
+              <Button onClick={handleSubmit} variant="contained">
+                Update
+              </Button>
+            </Grid>
+          </Grid>
+        </>
+      )}
     </Container>
   );
 }
