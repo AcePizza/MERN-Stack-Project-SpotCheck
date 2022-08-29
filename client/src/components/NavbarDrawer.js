f6580933d6c5396c3861922e217c5691c7167c37
AcePizza <stianchrbakke@gmail.com>
2022-08-29 14:08:47 +0200

    added resposive links

--- client/src/components/NavbarDrawer.js
+++ client/src/components/NavbarDrawer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
@@ -7,32 +7,128 @@ import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
+import { Link } from "@mui/material";
+import { useLocation } from "react-router-dom";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { AutenticationContext } from "../context/AutenticationContext";
 
 export default function NavbarDrawer({ openDrawer, setOpenDrawer }) {
-  //   const [state, setState] = useState(open);
-  console.log("open", openDrawer);
+  const currentPage = useLocation();
+  const { isUserLoggedIn } = useContext(AutenticationContext);
 
   const toggleDrawer = (open) => (event) => {
     setOpenDrawer(open);
   };
 
-  const list = () => (
+  const handleSortChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const notLoggedIn = () => (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
         <ListItem disablePadding>
           <ListItemButton>
-            <ListItemText>Something</ListItemText>
+            <Link href="/home" underline="none">
+              {"Home"}
+            </Link>
           </ListItemButton>
         </ListItem>
       </List>
       <Divider />
+      {currentPage.pathname == "/home" ? (
+        <List>
+          <ListItem>
+            <FormControl variant="standard" fullWidth>
+              <InputLabel id="sort-simple-select-label">Sort</InputLabel>
+              <Select
+                labelId="sort-simple-select-label"
+                id="sort-simple-select"
+                value={10}
+                label="sort"
+                onChange={handleSortChange}
+              >
+                <MenuItem value={10}>Ascending</MenuItem>
+                <MenuItem value={20}>Descending</MenuItem>
+              </Select>
+            </FormControl>
+          </ListItem>
+        </List>
+      ) : (
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText>You are NOT on the homepage</ListItemText>
+            </ListItemButton>
+          </ListItem>
+        </List>
+      )}
+    </Box>
+  );
+
+  const loggedIn = () => (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
         <ListItem disablePadding>
           <ListItemButton>
-            <ListItemText>Something</ListItemText>
+            <Link href="/home" underline="none">
+              {"Home"}
+            </Link>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <Link href="/profile" underline="none">
+              {"Profile"}
+            </Link>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <Link href="/addspot" underline="none">
+              {"Add Spot"}
+            </Link>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <Link href="/home" underline="none">
+              {"Logout"}
+            </Link>
           </ListItemButton>
         </ListItem>
       </List>
+      <Divider />
+      {currentPage.pathname == "/home" ? (
+        <List>
+          <ListItem>
+            <FormControl variant="standard" fullWidth>
+              <InputLabel id="sort-simple-select-label">Sort</InputLabel>
+              <Select
+                labelId="sort-simple-select-label"
+                id="sort-simple-select"
+                value={10}
+                label="sort"
+                onChange={handleSortChange}
+              >
+                <MenuItem value={10}>Ascending</MenuItem>
+                <MenuItem value={20}>Descending</MenuItem>
+              </Select>
+            </FormControl>
+          </ListItem>
+        </List>
+      ) : (
+        <List>
+          <ListItem disablePadding>
+            {/* <ListItemButton>
+              <ListItemText>{`Current Page: ${currentPage.pathname}`}</ListItemText>
+            </ListItemButton> */}
+          </ListItem>
+        </List>
+      )}
     </Box>
   );
 
@@ -40,7 +136,7 @@ export default function NavbarDrawer({ openDrawer, setOpenDrawer }) {
     <div>
       <>
         <Drawer anchor={"left"} open={openDrawer} onClose={toggleDrawer(false)}>
-          {list()}
+          {isUserLoggedIn ? notLoggedIn() : loggedIn()}
         </Drawer>
       </>
     </div>
