57ae8b6d758b852a72b60d7d8127798115e4d84b
AcePizza <stianchrbakke@gmail.com>
2022-08-30 15:59:30 +0200

    added display favorites and no. of favorites

--- client/src/components/MainSpotListCard.js
+++ client/src/components/MainSpotListCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -14,9 +14,29 @@ function MainSpotListCard({ spot, index }) {
   const redirectTo = useNavigate();
   const [favorite, setFavorite] = useState(false);
 
+  const isPostFavorited = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/spots/one/${spot._id}`
+      );
+      const result = await response.json();
+      if (result.votes.length <= 0) {
+        setFavorite(true);
+      } else {
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const favoriteButtonHandler = () => {
     setFavorite(!favorite);
   };
+
+  useEffect(() => {
+    isPostFavorited();
+  }, []);
 
   return (
     <React.Fragment key={index}>
@@ -40,8 +60,11 @@ function MainSpotListCard({ spot, index }) {
             onClick={favoriteButtonHandler}
             aria-label="add to favorites"
           >
-            {favorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+            {!favorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
           </IconButton>
+          <Typography variant="body2" color="text.secondary">
+            {spot.votes.length}
+          </Typography>
           <Button
             onClick={() => {
               redirectTo(`/spotdetails/${spot._id}`);
