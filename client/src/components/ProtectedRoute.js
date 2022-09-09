51223cf91821570ec4ae229ff81ccfa9962a752b
AcePizza <stianchrbakke@gmail.com>
2022-09-09 13:09:17 +0200

    refactoring

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import UserProfileView from "../views/UserProfileView";
 
@@ -6,7 +6,13 @@ function ProtectedRoute() {
   const isTheTokenThere = localStorage.getItem("token");
   const redirectTo = useNavigate();
 
-  return <>{isTheTokenThere ? <UserProfileView /> : redirectTo("/login")}</>;
+  console.log("isTokenThere", isTheTokenThere);
+
+  useEffect(() => {}, []);
+
+  return (
+    <>{isTheTokenThere === null ? redirectTo("/login") : <UserProfileView />}</>
+  );
 }
 
 export default ProtectedRoute;
